1672b28f0fa4f85a49dbf8caa51a0b95b9eefa50
nakagawa321 <54922044+nakagawa321@users.noreply.github.com>
2019-09-16 21:55:22 +0900

    commit
    add joystick

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+    public Joystick joystick;
+    public GameObject airplane;
+
+    void start()
+    {
+    }
+
+    void Update()
+    {
+
+        //joy stickの動き
+        Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.up * joystick.Vertical);
+
+        //Vector.zero : (0,0,0)なので、動いたら….
+
+        if (moveVector != Vector3.zero)
+        {
+
+            //移動…movespeedは移動速度のパラメータ。
+            airplane.transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
