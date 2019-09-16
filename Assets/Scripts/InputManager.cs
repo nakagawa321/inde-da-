d6ff599b7cece925eac6cd59c894d165cbbcb101
nakagawa321 <54922044+nakagawa321@users.noreply.github.com>
2019-09-17 00:01:14 +0900

    commit
    add stage

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    public float moveSpeed = 8f;
     public Joystick joystick;
     public GameObject airplane;
-
+    private float moveSpeed = 2f;
+    private float downLimit = -6.0f, upLimit = 6.6f;
+    private float undoPosition_Num = 0.05f;
     void start()
     {
     }
 
     void Update()
     {
+        if (downLimit < airplane.transform.position.y) // 下、場外に行かないように
+        {
+            joystickController(); // ジョイスティック動き
+        } else
+        {
+            airplane.transform.position = new Vector3(airplane.transform.position.x, downLimit + undoPosition_Num, airplane.transform.position.z);
+        }
+        if (airplane.transform.position.y < upLimit) // 上、場外に行かないように
+        {
+            joystickController(); // ジョイスティック動き
+        }
+        else
+        {
+            airplane.transform.position = new Vector3(airplane.transform.position.x, upLimit - undoPosition_Num, airplane.transform.position.z);
+        }
+    }
 
+    // ジョイスティック処理
+    void joystickController()
+    {
         //joy stickの動き
         Vector3 moveVector = (Vector3.right * joystick.Horizontal + Vector3.up * joystick.Vertical);
 
         //Vector.zero : (0,0,0)なので、動いたら….
-
         if (moveVector != Vector3.zero)
         {
-
             //移動…movespeedは移動速度のパラメータ。
             airplane.transform.Translate(moveVector * moveSpeed * Time.deltaTime, Space.World);
         }
     }
+
 }
