9ff7c3aea838f43e83422fc32d825540c6b2b662
nakagawa321 <54922044+nakagawa321@users.noreply.github.com>
2019-09-19 11:54:02 +0900

    commit
    add zikiHp

--- Assets/Scripts/enemyMove.cs
+++ Assets/Scripts/enemyMove.cs
@@ -22,8 +22,7 @@ public class enemyMove : MonoBehaviour
     // 当たると消える
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.name != "bullet(Clone)" || collision.gameObject.name != "ziki")
+        if (collision.gameObject.name == "bullet(Clone)" || collision.gameObject.name == "ziki")
         {
             Destroy(enemy);
         }
