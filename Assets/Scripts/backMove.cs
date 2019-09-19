64e04855b63fd07b8c83263d045732a339bdee62
nakagawa321 <54922044+nakagawa321@users.noreply.github.com>
2019-09-19 19:16:33 +0900

    commit
    add start&finish

---
+++ Assets/Scripts/backMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y < -14.0f)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, 14.0f, this.transform.position.z);
+        }
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.05f, this.transform.position.z);
+    }
+}
